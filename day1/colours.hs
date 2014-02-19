11f22eea2b1eadeb12f868efbba4345c66904c9c
Sleepy Fox <sleepyfox@gmail.com>
2014-02-19 17:14:29 +0000

    added HUnit tests for colours.hs

--- day1/colours.hs
+++ day1/colours.hs
@@ -1,4 +1,20 @@
 module Main where
+	import Test.HUnit
+
 	colours = ["black", "white", "blue", "yellow", "red"]
-	colour_combs :: [(String, String)]
+	colour_combs :: [(String, String)] 
 	colour_combs = [(x,y) | x <- colours, y <- colours, x <= y ]
+
+	results = colour_combs 
+
+	tests = TestList [ 
+		TestCase (assertEqual "results should contain (black, black)" 
+			True (elem ("black","black") results) ),
+		TestCase (assertEqual "results should contain (black, blue)" 
+			True (elem ("black","blue") results) ),
+		TestCase (assertEqual "results should NOT contain (blue, black)" 
+			False (elem ("blue","black") results) ),		
+		TestCase (assertEqual "results should contain 7 tuples"
+			15 (length results) ) ]
+
+	main = do runTestTT tests
