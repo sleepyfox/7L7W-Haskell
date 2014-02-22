ebe9d96bd10f093c83d4a4fdaa3c0f6f24342dd9
Sleepy Fox <sleepyfox@gmail.com>
2014-02-22 11:11:57 +0000

    added HUnit test to map_colouring.hs

--- day1/map_colouring.hs
+++ day1/map_colouring.hs
@@ -1,8 +1,15 @@
 module Main where
+	import Test.HUnit
+
 	states = ["Tennessee", "Mississippi", "Alabama", "Georgia", "Florida"]
 	colours = ["Red", "Green", "Blue"]
 	map_colours = [[("Tennessee", t),("Mississippi", m), ("Alabama",a),
 		("Georgia", g), ("Florida", f)] | 
 		m <- colours, t <- colours, a <- colours, g <- colours, f <- colours,
 		m /= t, m /= a, t /= a, t /= g, a /= g, a /= f, g /= f]
-	 
+
+	tests = TestList [ 
+		TestCase (assertEqual "There should be six possible variants" 
+			6 (length map_colours)),
+		TestCase (assertEqual "") ]	 
+	main = do runTestTT tests
